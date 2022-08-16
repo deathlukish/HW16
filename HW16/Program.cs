f3c8f500338668484dcff59c4efdb3ef31780763
admin <admin@BLACKQUEEN>
2022-08-16 12:49:51 +0300

    Добавьте файлы проекта.

---
+++ HW16/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW16
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            _ = PrintAsync(1000, "one");
+            _ = PrintAsync(2000, "two");
+            _ = PrintAsync(1500, "Three");
+            Console.ReadLine();
+
+        }
+
+
+        static async Task PrintAsync(int delay, string text)
+        {
+            await Task.Run(() =>
+            {
+                for(int i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(delay);
+                    Console.WriteLine($"{text} Поток: {Thread.CurrentThread.ManagedThreadId}");
+                   
+                }
+            });
+        }
+
+
+    }
+}
